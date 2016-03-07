b0915b4b077898c09f50a5626cf137185a349467
Himani Negi <hnegi150@gmail.com>
2016-03-07 14:31:13 +0530

    Create srn.cpp

---
+++ Schedulers/srn.cpp
@@ -0,0 +1,116 @@
+// Shortest request next
+
+# include <iostream>
+# include <fstream>
+# include <iomanip>
+
+using namespace std;
+
+struct process
+{
+   char name;
+   int at, st;
+   float wta,  wt, ta;
+   int time1;
+   int complete;
+}p[100];
+
+int min23;
+
+int q[100];  
+int front = -1, rear =- 1;
+
+void enqueue(int x)
+{
+   if(rear == 100)
+	  cout<<"Queue Overflow";
+
+   q[++rear] = x;
+
+   if(front == -1)
+	  front = 0;
+}
+
+int dequeue()
+{
+   if(front == -1)
+	  cout<<"Queue Underflow";
+
+   int temp = q[front];
+
+   if(front == rear)
+	  front = rear = -1;
+   else
+	  front++;
+  
+   return temp;
+}
+
+int main()
+{
+   int i, sum_st = 0;int j, k, min2;
+   char c;
+   float avg_wt; 
+   int time;
+   fstream in, out;
+   in.open("/home/himani/OperatingSystem/input.txt");
+   out.open("/home/himani/OperatingSystem/output.txt");
+
+   int n;
+
+   while(!in.eof())
+   {
+      in>>n;
+      for(i = 0; i < n; i++)
+  	     in>>p[i].at;
+   	  for(i = 0; i < n; i++)
+	     in>>p[i].st;
+   }
+   
+   for(i = 0, c = 'A'; i < n; i++, c++)
+   {
+      p[i].name = c;
+      sum_st += p[i].st;
+   }
+   for(i = 0; i < n; i++)
+   {
+	  cout<<p[i].name<<"   "<<p[i].at<<"   "<<p[i].st<<endl;
+   }
+
+   for(i = 0; i < n+1; i++)
+	  enqueue(i);
+
+   for(i = 0; i < n; i++)
+	  p[q[i]].complete = 0;
+
+   for(time = p[0].at; time < sum_st;)
+   { 
+   	  int min ;
+   	  min23 = 10;
+      
+   	  for(j = front; j < rear; j++ )
+      {
+	  	 if((p[q[j]].at == 0) && (p[q[j]].complete == 0))
+	  	 {
+	     	min = j;
+	  	 }
+	  	 if( (p[q[j]].complete == 0) && (p[q[j]].at != 0)&&  (p[j].at <= time) )
+	  	 {
+    	 	if( p[j].st <= min23)
+		 	{
+		       min2 = p[j].st;
+	 	 	   min = j;	
+	 		   min23 = p[j].st;			
+		    }
+	  	 }
+		 
+   	  }
+   	  p[0].time1 = 0;
+  
+   	  p[q[min]].complete = -1;
+   	  time += p[min].st;	
+
+   	  out<<time<<" "<<min+1<<endl;		
+   }  
+   return 0;	
+}
